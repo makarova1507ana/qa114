fba13b10bea3d541d612f473ef81741fad666d9a
Anastasia89038 <59653663+Anastasia89038@users.noreply.github.com>
2022-09-27 20:32:00 +0300

    Описаны действия пользователя ввод слова и нажатие на кнопку

--- POM/pages/yandexSearch/init.py
+++ POM/pages/yandexSearch/init.py
@@ -1,7 +1,12 @@
+from POM.browser import Browser
+from POM.pages.yandexSearch.LOCATORS import Locators
 
-class Searcher():
+class YandexSearcher(Browser):
     def enter_word(self, word):
-        pass
+        search_field = self.driver.find_element(Locators.search_field[0], Locators.search_field[1])
+        search_field.click()
+        search_field.send_keys(word)
+        return search_field
 
     def click_on_the_search_button(self):
-        pass
+        return self.driver.find_element(Locators.search_button[0], Locators.search_button[1])
