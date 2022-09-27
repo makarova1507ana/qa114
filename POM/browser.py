f4290fc37d235ab2794646f08e2c4b6d098d487c
Anastasia89038 <59653663+Anastasia89038@users.noreply.github.com>
2022-09-27 20:14:12 +0300

    Описан класс Browser добавлены функция открытия страницы по базовый ссылке добавлена функция закрытия браузера

--- POM/browser.py
+++ POM/browser.py
@@ -1,6 +1,13 @@
 from selenium import webdriver
 
 class Browser:
-    pass
-print("")
+    def __init__(self, base_url="https://ya.ru/"):
+        self.driver = webdriver.Chrome()
+        self.base_url = base_url
+
+    def go_to_site(self):
+        return self.driver.get(self.base_url)
+
+    def quit_from_browser(self):
+        return self.driver.quit()
 
