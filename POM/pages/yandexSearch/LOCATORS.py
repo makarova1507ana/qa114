4d6e3cab8cb3a95fc1df79538de3e91dd9511c34
Anastasia89038 <59653663+Anastasia89038@users.noreply.github.com>
2022-09-27 21:14:47 +0300

    Добавлен тест по кнопки очистить

--- POM/pages/yandexSearch/LOCATORS.py
+++ POM/pages/yandexSearch/LOCATORS.py
@@ -2,4 +2,4 @@ from selenium.webdriver.common.by import By
 class Locators:
     search_field = (By.ID, "text")
     search_button = (By.CLASS_NAME, "search3__button.mini-suggest__button")
-    chanel_button = (By.CLASS_NAME, "search3__svg_clear")
+    cancel_button = (By.CLASS_NAME, "search3__svg_clear")
--- POM/test.py
+++ POM/test.py
@@ -1,10 +1,20 @@
 import pytest
 from pages.yandexSearch.init import YandexSearcher
 import time
-def test_yandex_search():
+
+def test_search():
     yandex_page = YandexSearcher()
     yandex_page.go_to_site()
     yandex_page.enter_word("cat")
+    yandex_page.click_on_the_search_button()
+    time.sleep(2)
+    yandex_page.quit_from_browser()
+def test_enter_in_field_and_clear_words():
+    yandex_page = YandexSearcher()
+    yandex_page.go_to_site()
+    yandex_page.enter_word("cat")
+    yandex_page.click_on_cancel_button()
+    yandex_page.enter_word("dog")
     yandex_page.click_on_the_search_button()
     time.sleep(2)
     yandex_page.quit_from_browser()
